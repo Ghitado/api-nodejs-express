eaffc2e2e35eeb95ed3d03f2e275951c30f76b82
Ghitado <thiaguinho.020405@gmail.com>
2024-09-02 16:23:06 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'senha', 
+    database: 'usersdb'
+  });
+  
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log('Conectado ao banco de dados MySQL');
+  });
+  
+  // Middleware para analisar corpos de solicitação
+  app.use(bodyParser.json());
+  
+  app.post('/users', (req, res) => {
+    const { name, email } = req.body;
+    const INSERT_USER_QUERY = `INSERT INTO users (name, email) VALUES (?, ?)`;
+    connection.query(INSERT_USER_QUERY, [name, email], (err, results) => {
+      if (err) throw err;
+      res.send('Usuário criado com sucesso');
+    });
+  });
+  
+  app.get('/users', (req, res) => {
+    connection.query('SELECT * FROM users', (err, results) => {
+      if (err) throw err;
+      res.json(results);
+    });
+  });
+  
+  app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    const SELECT_USER_QUERY = `SELECT * FROM users WHERE id = ?`;
+    connection.query(SELECT_USER_QUERY, [userId], (err, results) => {
+      if (err) throw err;
+      res.json(results);
+    });
+  });
+  
+  app.delete('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    const DELETE_USER_QUERY = `DELETE FROM users WHERE id = ?`;
+    connection.query(DELETE_USER_QUERY, [userId], (err, results) => {
+      if (err) throw err;
+      res.json(results);
+    });
+  });  
+ 
+  app.put('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    const { name, email } = req.body;
+    const UPDATE_USER_QUERY = `UPDATE users set name=?, email=? where id=?`;
+    connection.query(UPDATE_USER_QUERY, [name, email,userId], (err, results) => {
+      if (err) throw err;
+      res.send('Usuário atualizado com sucesso');
+    });
+  });    
+
+  app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+  });
+  
