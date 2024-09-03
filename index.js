558909218bc6e4c3d5fddd8ea80756ea66d1e12f
Ghitado <thiaguinho.020405@gmail.com>
2024-09-03 17:31:09 -0300

    feat: criação do banco automático e início de teste das rotas

--- index.js
+++ index.js
@@ -5,66 +5,69 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'senha', 
-    database: 'usersdb'
-  });
-  
-  connection.connect((err) => {
-    if (err) throw err;
-    console.log('Conectado ao banco de dados MySQL');
-  });
-  
-  // Middleware para analisar corpos de solicitação
-  app.use(bodyParser.json());
-  
-  app.post('/users', (req, res) => {
-    const { name, email } = req.body;
-    const INSERT_USER_QUERY = `INSERT INTO users (name, email) VALUES (?, ?)`;
-    connection.query(INSERT_USER_QUERY, [name, email], (err, results) => {
-      if (err) throw err;
-      res.send('Usuário criado com sucesso');
-    });
-  });
-  
-  app.get('/users', (req, res) => {
-    connection.query('SELECT * FROM users', (err, results) => {
-      if (err) throw err;
-      res.json(results);
-    });
-  });
-  
-  app.get('/users/:id', (req, res) => {
-    const userId = req.params.id;
-    const SELECT_USER_QUERY = `SELECT * FROM users WHERE id = ?`;
-    connection.query(SELECT_USER_QUERY, [userId], (err, results) => {
-      if (err) throw err;
-      res.json(results);
-    });
-  });
-  
-  app.delete('/users/:id', (req, res) => {
-    const userId = req.params.id;
-    const DELETE_USER_QUERY = `DELETE FROM users WHERE id = ?`;
-    connection.query(DELETE_USER_QUERY, [userId], (err, results) => {
-      if (err) throw err;
-      res.json(results);
-    });
-  });  
- 
-  app.put('/users/:id', (req, res) => {
-    const userId = req.params.id;
-    const { name, email } = req.body;
-    const UPDATE_USER_QUERY = `UPDATE users set name=?, email=? where id=?`;
-    connection.query(UPDATE_USER_QUERY, [name, email,userId], (err, results) => {
-      if (err) throw err;
-      res.send('Usuário atualizado com sucesso');
-    });
-  });    
+require('dotenv').config()
 
-  app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Conectado ao banco de dados MySQL');
+});
+
+// Middleware para analisar corpos de solicitação
+app.use(bodyParser.json());
+
+app.post('/users', (req, res) => {
+  const { name, email } = req.body;
+  const INSERT_USER_QUERY = `INSERT INTO users (name, email) VALUES (?, ?)`;
+  connection.query(INSERT_USER_QUERY, [name, email], (err, results) => {
+    if (err) throw err;
+    res.send('Usuário criado com sucesso');
   });
-  
+});
+
+app.get('/users', (req, res) => {
+  connection.query('SELECT * FROM users', (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
+
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  const SELECT_USER_QUERY = `SELECT * FROM users WHERE id = ?`;
+  connection.query(SELECT_USER_QUERY, [userId], (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
+
+app.delete('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  const DELETE_USER_QUERY = `DELETE FROM users WHERE id = ?`;
+  connection.query(DELETE_USER_QUERY, [userId], (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});  
+
+app.put('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  const { name, email } = req.body;
+  const UPDATE_USER_QUERY = `UPDATE users set name=?, email=? where id=?`;
+  connection.query(UPDATE_USER_QUERY, [name, email,userId], (err, results) => {
+    if (err) throw err;
+    res.send('Usuário atualizado com sucesso');
+  });
+});    
+
+const server = app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
+
+module.exports = { app, server };
